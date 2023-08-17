84b92d3a7ff6af663f338ea42b0bad0b73e33ac6
tsbdivya <tsbdivya@gmail.com>
2023-08-17 11:14:31 -0700

    update subnets

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -17,15 +17,17 @@ resource "aws_default_vpc" "default" {
 
 }
 
-#data "aws_subnet_ids" "subnets" {
-#  vpc_id = aws_default_vpc.default.id
-#}
+data "aws_subnet_ids" "subnets" {
+  vpc_id = aws_default_vpc.default.id
+}
 
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint #module.in28minutes-cluster.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+#provider "kubernetes" {
+  //>>Uncomment this section once EKS is created - Start
+#  host                   = data.aws_eks_cluster.cluster.endpoint #module.in28minutes-cluster.cluster_endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#  token                  = data.aws_eks_cluster_auth.cluster.token
+#  //>>Uncomment this section once EKS is created - End
 }
 
 module "in28minutes-cluster" {
@@ -56,35 +58,34 @@ module "in28minutes-cluster" {
   }
 }
 
-# Uncomment this section once EKS is created - Start
-data "aws_eks_cluster" "cluster" {
-  name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
-}
+#//>>Uncomment this section once EKS is created - Start
+#data "aws_eks_cluster" "cluster" {
+#  name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
+#}
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
-}
+#data "aws_eks_cluster_auth" "cluster" {
+#  name = "in28minutes-cluster" #module.in28minutes-cluster.cluster_name
+#}
 
 
 # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
 # ServiceAccount needs permissions to create deployments 
 # and services in default namespace
-resource "kubernetes_cluster_role_binding" "example" {
-  metadata {
-    name = "fabric8-rbac"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "default"
-  }
-}
-# Uncomment this section once EKS is created - End
+#resource "kubernetes_cluster_role_binding" "example" {
+#  metadata {
+#    name = "fabric8-rbac"
+#  }
+#    api_group = "rbac.authorization.k8s.io"
+#    kind      = "ClusterRole"
+#    name      = "cluster-admin"
+#  }
+#  subject {
+#    kind      = "ServiceAccount"
+#    name      = "default"
+#    namespace = "default"
+#  }
+#}
+//>>Uncomment this section once EKS is created - End
 
 # Needed to set the default region
 provider "aws" {
